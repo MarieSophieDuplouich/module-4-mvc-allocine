ccd25fc7dcc4a4c228b8d1517f051bb1ee64b30c
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-11-10 12:01:10 +0100

    css avancé

--- app/views/user/profile.php
+++ app/views/user/profile.php
@@ -1,2 +1,16 @@
-<h1>User Profile </h1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="/public/css/style.css" class="css">
+    <title>user profile</title>
+</head>
+<body>
+    <h1>User Profile </h1>
+</body>
+</html>
+
+
+
 
