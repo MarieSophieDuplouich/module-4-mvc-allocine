5c263fed60eefc3c359326b14d060ad0bb207df1
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-11-10 11:10:04 +0100

    Slider 3d films bientôt finij

--- app/views/home.php
+++ app/views/home.php
@@ -1,19 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="app/public/css/style.css" class="css">
     <title>Allociné</title>
 </head>
-<body>
-   <h1>Home Page Allociné</h1> 
-   <div class="box">
-    <div class="item"></div>
-     <div class="item"></div>
-      <div class="item"></div>
-       <div class="item"></div>
-   </div>
-</body>
-</html>
 
+<body>
+    <h1>Home Page Allociné</h1>
+    <div class="box">
+        <div class="item"><img src="http://unsplash.it/100/100" alt=""></div>
+        <div class="item"><img src="http://unsplash.it/100/100" alt=""></div>
+        <div class="item"><img src="http://unsplash.it/100/100" alt=""></div>
+        <div class="item"><img src="http://unsplash.it/100/100" alt=""></div>
+        <div class="item"><img src="http://unsplash.it/100/100" alt=""></div>
+        <div class="item"><img src="http://unsplash.it/100/100" alt=""></div>
+        <div class="item"><img src="http://unsplash.it/100/100" alt=""></div>
+    </div>
+    <div class="buttons">
+        <span class="prev"></span>
+         <span class="next"></span>
+    </div>
+</body>
+
+</html>
