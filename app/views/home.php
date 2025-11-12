d6781fb351e9f05fc6a40c10a76fa4233e9d1c01
MarieSophieDuplouich <ms.duplouichiscod@gmail.com>
2025-11-12 15:04:19 +0100

    Search bar commencé

--- app/views/home.php
+++ app/views/home.php
@@ -5,13 +5,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/public/css/style.css" class="css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title>Accueil Allociné</title>
 </head>
 
 <body>
     <header>
-     <h1>Accueil Allociné</h1>
-    
+     <h1>Allociné</h1>
+    <div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#about">About</a>
+  <a href="#contact">Contact</a>
+  <div class="search-container">
+    <form action="/action_page.php">
+      <input type="text" placeholder="Search.." name="search">
+      <button type="submit"><i class="fa fa-search"></i></button>
+    </form>
+  </div>
+</div>
+
+
+
     </header>
    
 
